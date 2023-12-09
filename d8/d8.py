ef17e9f074610fb8ac079c2d046eb8f535d51f94
Alexander Bollaert <alexander.bollaert@gmail.com>
2023-12-09 12:34:50 +0100

    Day 8, part 2, and day 9.

--- d8/d8.py
+++ d8/d8.py
@@ -1,33 +1,68 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from dataclasses import field
 from typing import List
 from typing import Dict
-from typing import Tuple
+from math import lcm
 
 @dataclass
 class Trip:
-    location: str
+    start_locations: List[str] = field(init=False)
+    locations: List[str] = field(init=False)
+    steps_to_reach_end: Dict[str, int] = field(init=False)
     map: Map
 
-    def walk(self) -> List[Tuple[str, str]]:
-        path: List[Tuple[str, str]] = []
+    def walk(self) -> int:
+        self.locations = self.map.find_start_points()
+        self.start_locations = self.locations
+        self.steps_to_reach_end = {}
+
+        for location in self.locations:
+            self.steps_to_reach_end[location] = 0
+
+        print("Map has %d starting points." % (len(self.locations)))
 
         arrived: bool = False
+        num_steps: int = 0
 
         while not arrived:
             for i in range(0, len(map.directions)):
-                node: Node = map.nodes[self.location]
-                next_node: str = node.left if map.directions[i] == "L" else node.right
+                arrived = self.__step(map.directions[i])
+                num_steps = num_steps + 1
 
-                self.location = next_node
-
-                path.append((map.directions[i], self.location))
-
-                if self.location == "ZZZ":
-                    arrived = True
+                if arrived:
                     break
 
-        return path
+        return lcm(*self.steps_to_reach_end.values())
+
+    def __step(self, direction: str) -> bool:
+        new_locations: List[Node] = []
+
+        for i in range(0, len(self.locations)):
+            current_location: str = self.locations[i]
+
+            if not self.map.nodes[current_location].is_end_node():
+                start_location: str = self.start_locations[i]
+
+                node: Node = map.nodes[current_location]
+                next_node = self.map.nodes[node.left] if direction == "L" else self.map.nodes[node.right]
+
+                new_locations.append(next_node)
+
+                self.steps_to_reach_end[start_location] = self.steps_to_reach_end[start_location] + 1
+            else:
+                new_locations.append(self.map.nodes[current_location])
+
+        self.locations = [location.name for location in new_locations]
+
+        for i in range(0, len(new_locations)):
+            new_location = new_locations[i]
+
+            if not new_location.is_end_node():
+                return False
+
+        return True
+
 @dataclass
 class Node:
     name: str
@@ -46,6 +81,13 @@ class Node:
         right: str = next_parts[1].replace(")", "").strip()
 
         return Node(name, left, right)
+
+    def is_start_node(self):
+        return self.name.endswith("A")
+
+    def is_end_node(self):
+        return self.name.endswith("Z")
+
 @dataclass
 class Map:
     directions: List[str]
@@ -66,12 +108,20 @@ class Map:
 
         return Map(directions, nodes)
 
+    def find_start_points(self) -> List[str]:
+        start_points: List[str] = []
+
+        for node in self.nodes.values():
+            if node.is_start_node():
+                start_points.append(node.name)
+
+        return start_points
+
 if __name__ == "__main__":
     with open("input.txt") as data_file:
         map: Map = Map.parse(data_file.readlines())
-        trip: Trip = Trip("AAA", map)
+        trip: Trip = Trip(map)
 
-        steps: List[Tuple[str, str]] = trip.walk()
+        steps: int = trip.walk()
 
         print(steps)
-        print(len(steps))
---
+++ d9/d9.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from dataclasses import field
+from typing import List
+
+
+@dataclass
+class ValueHistory:
+    values: List[int]
+    derived: ValueHistory
+    parent: ValueHistory
+
+    @staticmethod
+    def parse(line: str) -> ValueHistory:
+        value_history: ValueHistory = ValueHistory([int(value) for value in line.split()], None, None)
+        value_history.derive()
+
+        return value_history
+
+    def __derive_difference_sequence(self) -> ValueHistory:
+        differences: List[int] = []
+
+        for i in range(1, len(self.values)):
+            differences.append(self.values[i] - self.values[i - 1])
+
+        return ValueHistory(differences, None, self)
+
+    def __is_all_zeroes(self):
+        for v in self.values:
+            if v != 0:
+                return False
+
+        return True
+
+    def derive(self):
+        current: ValueHistory = self
+        current.derived = current.__derive_difference_sequence()
+
+        while not current.derived.__is_all_zeroes():
+            current = current.derived
+            current.derived = current.__derive_difference_sequence()
+
+    def next_value(self):
+        current: ValueHistory = self
+
+        while current.derived is not None:
+            current = current.derived
+
+        if current.__is_all_zeroes():
+            current.values.append(0)
+            current = current.parent
+
+        while current.parent is not None:
+            current.values.append(current.values[len(current.values) - 1] + current.derived.values[len(current.derived.values) - 1])
+            current = current.parent
+
+        current.values.append(current.values[len(current.values) - 1] + current.derived.values[len(current.derived.values) - 1])
+
+    def previous_value(self):
+        current: ValueHistory = self
+
+        while current.derived is not None:
+            current = current.derived
+
+        if current.__is_all_zeroes():
+            current.values.insert(0, 0)
+            current = current.parent
+
+        while current.parent is not None:
+            current.values.insert(0, current.values[0] - current.derived.values[0])
+            current = current.parent
+
+        current.values.insert(0, current.values[0] - current.derived.values[0])
+
+if __name__ == "__main__":
+    with open("input.txt") as data_file:
+        value_histories: List[ValueHistory] = []
+
+        for line in data_file.readlines():
+            value_histories.append(ValueHistory.parse(line))
+
+        # print(value_histories)
+
+        sum: int = 0
+
+        for value_history in value_histories:
+            value_history.previous_value()
+            print(value_history)
+            sum = sum + value_history.values[0]
+
+        print(sum)
